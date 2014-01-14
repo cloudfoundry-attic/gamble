ae9fd277c30c0f8de939d18e7fb4d1896db61a5c
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-01-13 16:29:59 -0800

    Fix incorrect enum type

--- yaml_extensions.h
+++ yaml_extensions.h
@@ -7,7 +7,7 @@ static const char * yaml_event_get_scalar_value(yaml_event_t *event) {
     return (const char *)event->data.scalar.value;
 }
 
-static yaml_event_type_t yaml_event_get_scalar_style(yaml_event_t *event) {
+static yaml_scalar_style_t yaml_event_get_scalar_style(yaml_event_t *event) {
     return event->data.scalar.style;
 }
 
