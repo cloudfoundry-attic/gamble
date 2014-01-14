38b9039241410a90a88ce9fb9488bc6ca2d6d4ed
Max Brunsfeld <mbrunsfeld@pivotallabs.com>
2014-01-14 08:56:54 -0800

    Remove unnecessary recursion from main parsing loop
    We only need to recur when starting a mapping or a sequence

--- gamble.go
+++ gamble.go
@@ -29,48 +29,48 @@ func Parse(input string) (result Node, err error) {
 }
 
 func getNode(p *C.yaml_parser_t, stopEvent C.yaml_event_type_t) Node {
-	var event C.yaml_event_t
-	if int(C.yaml_parser_parse(p, &event)) == 0 {
-		panic(errors.New("Error parsing YAML."))
-	}
+	for {
+		var event C.yaml_event_t
+		if int(C.yaml_parser_parse(p, &event)) == 0 {
+			panic(errors.New("Error parsing YAML."))
+		}
 
-	eventType := event._type
-	if eventType == stopEvent {
-		return nil
-	}
-
-	switch eventType {
-	case C.YAML_SCALAR_EVENT:
-		str := C.GoString(C.yaml_event_get_scalar_value(&event))
-		if str == "null" && C.yaml_event_get_scalar_style(&event) == C.YAML_PLAIN_SCALAR_STYLE {
+		eventType := event._type
+		if eventType == stopEvent {
 			return nil
-		} else {
-			return str
 		}
-	case C.YAML_SEQUENCE_START_EVENT:
-		sequenceNode := make([]interface{}, 0)
-		for {
-			item := getNode(p, C.YAML_SEQUENCE_END_EVENT)
-			if item != nil {
-				sequenceNode = append(sequenceNode, item)
-			} else {
-				break
-			}
-		}
-		return sequenceNode
-	case C.YAML_MAPPING_START_EVENT:
-		mappingNode := make(map[string]interface{})
-		for {
-			key := getNode(p, C.YAML_MAPPING_END_EVENT)
-			if key != nil {
-				value := getNode(p, C.YAML_MAPPING_END_EVENT)
-				mappingNode[key.(string)] = value
-			} else {
-				break
-			}
-		}
-		return mappingNode
-	}
 
-	return getNode(p, stopEvent)
+		switch eventType {
+		case C.YAML_SCALAR_EVENT:
+			str := C.GoString(C.yaml_event_get_scalar_value(&event))
+			if str == "null" && C.yaml_event_get_scalar_style(&event) == C.YAML_PLAIN_SCALAR_STYLE {
+				return nil
+			} else {
+				return str
+			}
+		case C.YAML_SEQUENCE_START_EVENT:
+			sequenceNode := make([]interface{}, 0)
+			for {
+				item := getNode(p, C.YAML_SEQUENCE_END_EVENT)
+				if item != nil {
+					sequenceNode = append(sequenceNode, item)
+				} else {
+					break
+				}
+			}
+			return sequenceNode
+		case C.YAML_MAPPING_START_EVENT:
+			mappingNode := make(map[string]interface{})
+			for {
+				key := getNode(p, C.YAML_MAPPING_END_EVENT)
+				if key != nil {
+					value := getNode(p, C.YAML_MAPPING_END_EVENT)
+					mappingNode[key.(string)] = value
+				} else {
+					break
+				}
+			}
+			return mappingNode
+		}
+	}
 }
